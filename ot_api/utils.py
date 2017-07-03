637baffa5ff6972887ca4909910920765a2d92e2
Jan Waś <janwas@tjsoftware.co>
2017-07-03 10:53:03 +0200

    allow to use a different base url

--- ot_api/utils.py
+++ ot_api/utils.py
@@ -1,12 +1,14 @@
 from ot_api.endpoints import BASE_URL
 
 
-def build_endpoint_url(account_name, endpoint_name):
+def build_endpoint_url(account_name, endpoint_name, base_url=None):
     """
     build endpoint url based on account_name and api_name
     :param account_name: name of the account in opentopic - actually it's a slug available in url address
     :param endpoint_name: api name that you want to get in to (just a path to api endpoint)
+    :param base_url: base url
     :return: str with full url to api endpoint
     """
-
-    return BASE_URL.format(account_name=account_name, endpoint_name=endpoint_name)
+    if base_url is None:
+        base_url = BASE_URL
+    return base_url.format(account_name=account_name, endpoint_name=endpoint_name)
